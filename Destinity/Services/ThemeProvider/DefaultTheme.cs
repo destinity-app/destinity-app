8c2214ddb3a0c34b0087f1057e981ab7b2ecf35b
Morasiu <morasiu2@gmail.com>
2023-02-03 17:41:33 +0100

    Loading

--- Destinity/Services/ThemeProvider/DefaultTheme.cs
+++ Destinity/Services/ThemeProvider/DefaultTheme.cs
@@ -6,7 +6,11 @@ public class DefaultTheme {
 	public static MudTheme Value { get; } = new() {
 		Palette = new Palette {
 			AppbarBackground = Colors.Shades.White,
-			AppbarText = Colors.Shades.Black
+			AppbarText = Colors.Shades.Black,
+			Primary = new MudColor("#B113F6")
+		},
+		PaletteDark = new PaletteDark() {
+			Primary = new MudColor("#B113F6")
 		}
 	};
 }
